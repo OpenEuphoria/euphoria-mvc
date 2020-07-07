1721f7820389a74f4758e8b3d13b641593adf3ec
Greg Haberek <ghaberek@gmail.com>
2020-07-06 22:52:01 -0400

    Minor update to example4.ex

--- examples/example4.ex
+++ examples/example4.ex
@@ -5,6 +5,8 @@ include mvc/server.e
 include mvc/template.e
 include std/map.e
 
+set_template_path( "../templates" )
+
 function user_id( object request )
     
     integer id = map:get( request, "id", -1 )
@@ -33,11 +35,12 @@ function index( object request )
     
     map response = map:new()
     map:put( response, "title", "Index" )
+    map:put( response, "message", "Hello, world!" )
     
     return render_template( "index.html", response )
 end function
 app:route( "/index", "index" )
-app:route( "/", "root", routine_id("index") )
+app:route( "/", "root",, routine_id("index") )
 
 set_log_level( LOG_ALL )
 server:start()
